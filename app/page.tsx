0123a1cb3b3d326989928a2e1e56a01028d05bce
Joel Hellberg <joehe737@student.liu.se>
2024-06-19 22:17:24 +0200

    Städat upp lite i koden

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,16 @@ import ContactCard from '../components/ContactCard'
 export default function Home() {
   return (
     <div className="flex-1">
-      <div className="relative flex items-center w-full h-[90vh] mobile:grid mobile:h-auto mobile:w-full p-8 handheld:p-0 pb-12">
+      <div className="relative flex items-center w-full h-[90vh] p-8 pb-12
+      handheld:p-0
+      mobile:grid mobile:h-auto mobile:w-full">
 
         {/* Green square positioned behind the image, only visible on mobile */}
-        <div className="hidden mobile:block mobile:aspect-square mobile:absolute mobile:right-0 mobile:top-0 mobile:w-4/6 mobile:bg-darkLobster"></div>
-        <div className="relative mobile:aspect-square mobile:w-1/12"></div>
+        <div className="hidden  mobile:bg-darkLobster
+        mobile:block mobile:aspect-square mobile:absolute mobile:right-0 mobile:top-0 mobile:w-4/6"/>
+
+        <div className="relative mobile:aspect-square mobile:w-1/12" />
+
         {/* Left Image Container */}
         <div className="relative w-1/2 shadow-lg mobile:w-4/5 mobile:mx-auto">
           {/* Left Image */}
@@ -47,12 +52,12 @@ export default function Home() {
         <div className="absolute w-1/3 right-1/4 mobile:relative handheld:w-3/5 mobile:w-11/12 mobile:py-8 mobile:mx-auto handheld:right-0" >
           <div className="bg-background p-10 text-center shadow-lg">
             <h2 className="text-left">Basåret.</h2>
-            <p className="text-left pb-5">Glöm inte att fylla i nolle-enkäten! Följ oss gärna också på Instagram för att ta del av fler uppdateringar!.</p>
+            <p className="text-left pb-5">Glöm inte att fylla i nolle-enkäten! Följ oss gärna också på Instagram för att ta del av fler uppdateringar!</p>
             <div className="flex space-x-4">
               <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
                 <button className="bg-darkPurple text-white px-4 py-2 rounded h-11 flex items-center justify-center">Nolle-enkäten</button>
               </a>
-              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+              <a href="https://www.instagram.com/basaret_norrkoping/">
                 <button className="bg-darkOrange text-white px-4 py-2 rounded h-11 flex items-center justify-center">
                   <div className="w-full h-full flex items-center justify-center">
                     <Image
@@ -72,7 +77,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex w-full bg-blue-500 mobile:bg-slate-100 pt-10 mobile:pt-0">
+      <div className="flex w-full mobile:bg-slate-100 pt-10 mobile:pt-0">
 
         {/* Welcome */}
         <div className="w-4/6
