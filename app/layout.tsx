e9578099396bf19ec30d1249e8c560bec743cd47
Joel Hellberg <joehe737@student.liu.se>
2024-08-18 12:24:44 +0200

    Lade till Voyado som sponsor och tog bort "test"

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,8 @@ export default function RootLayout({
         handheld:px-20 handheld:flex-col handheld:space-y-10
         mobile:px-8      
         ">
-          <Sponsor company1="ICA" company2="" className="grow-1 h-52 w-2/3 mobile:w-full" />
+          <Sponsor company1="ICA" company2="" className="grow-1 h-52 w-1/3 mobile:w-full" />
+          <Sponsor company1="Voyado" company2="" className="grow-1 h-48 w-1/3 mobile:w-4/5" />
           {/*<Sponsor company1="NAB" company2="Voyado" className="grow-1 h-48 w-1/3 mobile:w-3/5" />
           <Sponsor company1="Micropower" company2="" className="grow-2 h-48 w-2/3 mobile:w-full" />*/}
         </div>
